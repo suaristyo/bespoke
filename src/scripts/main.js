ad062009b6b17d26c07128b857cd9d959463f8bd
171112640 <171112640@students.mikroskil.ac.id>
2020-12-20 22:05:01 +0700

    Applied Themes

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,14 +7,20 @@ var bullets = require('bespoke-bullets');
 var hash = require('bespoke-hash');
 var prism = require('bespoke-prism');
 var extern = require('bespoke-extern');
+var backdrop = require('bespoke-backdrop');
+var nebula = require('bespoke-theme-nebula');
+var progress = require('bespoke-progress');
 
 // Bespoke.js
 bespoke.from({ parent: 'article.deck', slides: 'section' }, [
+  nebula(),
   classes(),
   nav(),
   scale(),
   bullets('.build, .build-items > *:not(.build-items)'),
   hash(),
   prism(),
-  extern(bespoke)
+  extern(bespoke),
+  backdrop(),
+  progress()
 ]);
