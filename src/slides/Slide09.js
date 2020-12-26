98c4773f6ae1fa8054e1c97700cedf5eda78116e
Suaristyo Salihin <suaristyo@gmail.com>
2020-12-26 16:47:47 +0700

    Penambahan slide09

---
+++ src/slides/Slide09.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Slide09 = () => (
+  <section>
+    <h2 style={{color : "white"}}>Kesalahan Umum yang dilakukan saat Interview</h2>
+    <br/>
+    <ol>
+        <li>Menjelekkan kantor sebelumnya</li>
+        <li>Tidak mempersiapkan diri sebelumnya</li>
+        <li>Tidak tepat waktu atau terlambat</li>
+        <li>Tidak memperlihatkan ketertarikan</li>
+        <li>Terlalu keras mengeluarkan suara</li>
+        <li>Bersikap cuek pada saat melakukan wawancara</li>
+    </ol>
+
+    <br />
+    
+  </section>
+);
+
+export default Slide09;
