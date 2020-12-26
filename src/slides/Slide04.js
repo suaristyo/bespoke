bdc48944cabf2a5976325c63568af651f413c1ef
WilliamHandono <williamk753@gmail.com>
2020-12-26 20:58:05 +0700

    add to slide 4

--- src/slides/Slide04.js
+++ src/slides/Slide04.js
@@ -2,12 +2,13 @@ import React from 'react';
 
 const Slide04 = () => (
   <section>
-    <h2 className="test">Keuntungan Apa Yang Akan Didapatkan Oleh Perusahaan tersebut</h2>
+    <h2 style={{color : "white" ,fontSize : "38px"}}>Keuntungan Apa Yang Akan Didapatkan Oleh Perusahaan tersebut ?</h2>
     <br />
     <ol>
-      <li>Onea</li>
-      <li>Two</li>
-      <li>Three</li>
+      <li>Selalu memperhatikan kebutuhan apa yang sedang dicari oleh perusahaan dan pewawancara</li>
+      <li>Selalu memberikan jawaban yang relevan dengan pekerjaan yang sedang dibahas</li>
+      <li>Jika pekerjaan tersebut tidak cocok dengan anda , mungkin mereka bisa memiliki lowongan lain yang lebih cocok untuk anda</li>
+      <li>Sanjung / Memuji mereka  (Perusahaan yang bagus,Kesempatan yang baik ,pertanyaan yang bagus</li>
     </ol>
   </section>
 );
