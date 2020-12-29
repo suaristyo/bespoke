0070ec4841f2a0d0a96b845a846dc01470933853
WilliamHandono <williamk753@gmail.com>
2020-12-29 22:49:38 +0700

    Dokumen yang diperlukan dalam Interivwe (Slide15)

---
+++ src/slides/Slide15.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+
+const Slide15 = () => {
+  
+  return (
+    <section>
+    <h2 style={{color : "white"}}>Dokumen apa saja yang diperlukan ?</h2>
+    <br/>
+    <ol>
+        <li>CV (Curriculum Vitae) ,Resume dan Bio Data</li>
+        <li>Sertifikat Pendidikan</li>
+        <li>Sertifikat Kegiatan Ekstrakurikuler</li>
+        <li>Sertifikat Pengalaman</li>
+        <li>Surat Penghargaan</li>
+        <li>Referensi</li>
+    </ol>
+    <br />
+  </section>
+  );
+}
+
+export default Slide15;
