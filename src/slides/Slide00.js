060b441bec233a72ea6fb7de7c4be3e71c3c9441
171112640 <171112640@students.mikroskil.ac.id>
2020-12-30 22:26:53 +0700

    Other Sample Animation

--- src/slides/Slide00.js
+++ src/slides/Slide00.js
@@ -4,17 +4,30 @@ import styled from 'styled-components';
 const Div = styled.div`
   width: 100px;
   height: 100px;
-  background: red;
-  transition: width 2s;
-  &:hover {
-    width: 300px;
+  background-color: red;
+  position: relative;
+  animation-name: block;
+  animation-duration: 4s;
+  animation-iteration-count: infinite;
+  @keyframes block {
+    0%   {background-color:red; left:0px; top:0px;}
+    25%  {background-color:yellow; left:200px; top:0px;}
+    50%  {background-color:blue; left:200px; top:200px;}
+    75%  {background-color:green; left:0px; top:200px;}
+    100% {background-color:red; left:0px; top:0px;}
  }
+`;
+
+const Paragraph = styled.p`
+  font-size: 15px;
+  text-align: center;
 `;
 
 const Slide15 = () => {
   return (
     <section>
-    <Div></Div>
+    <Div>
+    </Div>
     </section>
   );
 }
--- src/slides/Slide13.js
+++ src/slides/Slide13.js
@@ -1,11 +1,21 @@
 import React, {useState} from 'react';
 import ReactImg from '../static/tips_trick_int.jpg';
+import styled from 'styled-components';
 
+const Img =styled.img`
+  width: 100px;
+  height: 100px;
+  background: red;
+  transition: width 2s linear 1s;
+  &:active {
+    width: 300px;
+  }
+`;
 
 const Slide13 = () => {
   return (
     <section>
-      <img src={ReactImg} style={{ height: 300 }}></img>
+      <Img src={ReactImg} style={{ height: 300 }}></Img>
       <br/>
       <h1 style={{color : "white"}}>Tips and Trick Interview !!</h1>
   </section>
