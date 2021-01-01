93adb0840c9c048e104d1e9f49b76168c49a4db2
171112640 <171112640@students.mikroskil.ac.id>
2021-01-01 13:11:31 +0700

    Adding Style Slide01

--- src/slides/Slide01.js
+++ src/slides/Slide01.js
@@ -1,21 +1,92 @@
-import React, { useState } from 'react';
-import { Button , DatePicker, List} from 'antd';
-import 'antd/dist/antd.css';
-import Item from 'antd/lib/list/Item';
+import React from 'react';
+import styled from 'styled-components';
 
+const PA =styled.p`
+color:white;
+animation-name: flip;
+animation-duration: 4s;
+@keyframes flip {
+  from {
+    -webkit-transform: perspective(400px) scale3d(1, 1, 1) translate3d(0, 0, 0) rotate3d(0, 1, 0, -360deg);
+    transform: perspective(400px) scale3d(1, 1, 1) translate3d(0, 0, 0) rotate3d(0, 1, 0, -360deg);
+    -webkit-animation-timing-function: ease-out;
+    animation-timing-function: ease-out;
+    opacity: 0;
+  }
+
+  40% {
+    -webkit-transform: perspective(400px) scale3d(1, 1, 1) translate3d(0, 0, 150px)
+      rotate3d(0, 1, 0, -190deg);
+    transform: perspective(400px) scale3d(1, 1, 1) translate3d(0, 0, 150px)
+      rotate3d(0, 1, 0, -190deg);
+    -webkit-animation-timing-function: ease-out;
+    animation-timing-function: ease-out;
+  }
+
+  50% {
+    -webkit-transform: perspective(400px) scale3d(1, 1, 1) translate3d(0, 0, 150px)
+      rotate3d(0, 1, 0, -170deg);
+    transform: perspective(400px) scale3d(1, 1, 1) translate3d(0, 0, 150px)
+      rotate3d(0, 1, 0, -170deg);
+    -webkit-animation-timing-function: ease-in;
+    animation-timing-function: ease-in;
+  }
+
+  80% {
+    -webkit-transform: perspective(400px) scale3d(0.95, 0.95, 0.95) translate3d(0, 0, 0)
+      rotate3d(0, 1, 0, 0deg);
+    transform: perspective(400px) scale3d(0.95, 0.95, 0.95) translate3d(0, 0, 0)
+      rotate3d(0, 1, 0, 0deg);
+    -webkit-animation-timing-function: ease-in;
+    animation-timing-function: ease-in;
+  }
+
+  to {
+    -webkit-transform: perspective(400px) scale3d(1, 1, 1) translate3d(0, 0, 0) rotate3d(0, 1, 0, 0deg);
+    transform: perspective(400px) scale3d(1, 1, 1) translate3d(0, 0, 0) rotate3d(0, 1, 0, 0deg);
+    -webkit-animation-timing-function: ease-in;
+    animation-timing-function: ease-in;
+    opacity: 1;
+  }
+}
+`;
+const H3 =styled.h3`
+color:white;
+animation-name: fadeIn;
+animation-duration: 4s;
+@keyframes fadeIn {
+  from {
+    opacity: 0;
+  }
+
+  to {
+    opacity: 1;
+  }
+}
+`;
+const H1 =styled.h1`
+color:white;
+animation-name: fadeIn;
+animation-duration: 4s;
+@keyframes fadeIn {
+  from {
+    opacity: 0;
+  }
+
+  to {
+    opacity: 1;
+  }
+}
+`;
 
 const Slide01 = () => {
-  const [startDate ,setStartDate] = useState(new Date());
-  console.log(startDate)
   return (
     <section>
-    <h1 style={{color:"white"}}>Presentasi Interview T&T</h1>
-    <h3 style={{color:"white"}}>Anggota</h3>
-    <p>Stephen (171112640)</p>
-    <p>Suaristyo (171111296)</p>
-    <p>William Handono (171110258)</p>
-
-    <DatePicker  id = "dt" selected={startDate} onChange={date => setStartDate(date)}/>
+    <H1>Presentasi Interview T&T</H1>
+    <H3>Anggota</H3>
+    <PA>Stephen (171112640)</PA>
+    <PA>Suaristyo (171111296)</PA>
+    <PA>William Handono (171110258)</PA>
     <br />
   </section>
   );
